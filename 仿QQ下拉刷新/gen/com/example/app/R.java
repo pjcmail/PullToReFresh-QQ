10c0d8ed7215391eef3fd95c8fceb56558a77a83
pjc <pjcmail@163.com>
2015-06-07 15:13:14 +0800

    第二次提交，完善了界面，增加了top和bottom布局

--- 仿QQ下拉刷新/gen/com/example/app/R.java
+++ 仿QQ下拉刷新/gen/com/example/app/R.java
@@ -26,14 +26,18 @@ public final class R {
         public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
-        public static final int image=0x7f020001;
-        public static final int loading=0x7f020002;
-        public static final int pull_icon_big=0x7f020003;
-        public static final int refresh_fail=0x7f020004;
-        public static final int refresh_failed=0x7f020005;
-        public static final int refresh_succeed=0x7f020006;
-        public static final int refreshing=0x7f020007;
+        public static final int bg=0x7f020000;
+        public static final int bg_bottom=0x7f020001;
+        public static final int bg_scroll=0x7f020002;
+        public static final int dxu=0x7f020003;
+        public static final int ic_launcher=0x7f020004;
+        public static final int image=0x7f020005;
+        public static final int loading=0x7f020006;
+        public static final int pull_icon_big=0x7f020007;
+        public static final int refresh_fail=0x7f020008;
+        public static final int refresh_failed=0x7f020009;
+        public static final int refresh_succeed=0x7f02000a;
+        public static final int refreshing=0x7f02000b;
     }
     public static final class id {
         public static final int action_settings=0x7f090008;
@@ -48,7 +52,9 @@ public final class R {
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
-        public static final int refresh_head=0x7f030001;
+        public static final int bottom=0x7f030001;
+        public static final int refresh_head=0x7f030002;
+        public static final int top=0x7f030003;
     }
     public static final class menu {
         public static final int main=0x7f080000;
